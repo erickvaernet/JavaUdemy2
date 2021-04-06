30075cc0c35d9914fac6e18318d9ca93c11e9568
erick <erickadrielnet@gmail.com>
2021-04-06 09:36:10 -0300

    0

---
+++ Q1IntroJDBC/Main1.java
@@ -0,0 +1,10 @@
+package Q1IntroJDBC;
+
+public class Main1 {
+    public static void main(String[] args) {
+        /*
+        SQL es Structered Query Language,lenguaje de consultas estructurado, se usa para administrar y acceder a un
+        sistema de gestion de base de datos relacionales (RDBMS)
+         */
+    }
+}
